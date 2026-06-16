4c7fa900602de3ab510f1b051b16caf3a4f1b55e
prachipandey28 <prachipandey1528@gmail.com>
2026-06-16 18:17:16 +0530

    commit

--- main.py
+++ main.py
@@ -1,38 +1,34 @@
-'''
-1 for snake
--1 for water
-0 for gun
-'''
+import random
 
-computer =-1
-youstr = input("enter your choice :")
-youDict={"s":1,"w":-1,"g":0}
-reverseDict={0:"g",-1:"w",1:"s"}
+# 1 = Snake
+# -1 = Water
+# 0 = Gun
 
-you=youDict[youstr]
+youDict = {"s": 1, "w": -1, "g": 0}
+reverseDict = {1: "Snake", -1: "Water", 0: "Gun"}
 
-print(f"you chose :{reverseDict[you]}\ncomputer choose :{reverseDict[computer]}")
+computer = random.choice([1, -1, 0])
 
-if(computer==you):
-    print("its a draw")
+youstr = input("Enter your choice (s/w/g): ").lower()
+
+if youstr not in youDict:
+    print("❌ Invalid choice! Enter s, w, or g.")
+    exit()
+
+you = youDict[youstr]
+
+print(f"\nYou chose: {reverseDict[you]}")
+print(f"Computer chose: {reverseDict[computer]}")
+
+if computer == you:
+    print("🤝 It's a draw!")
+
+elif (
+    (you == 1 and computer == -1) or
+    (you == -1 and computer == 0) or
+    (you == 0 and computer == 1)
+):
+    print("🎉 You win!")
 
 else:
-    if(computer==-1 and you ==1):
-        print("you win!")
-
-    elif(computer==-1 and you ==0):
-        print("you loss!")
-
-    elif(computer==1 and you ==-1):
-        print("you loss!" )
-          
-    elif(computer==1 and you ==0):
-        print("you win!")
-
-    elif(computer==0 and you ==-1):
-        print("you win!")
-
-    elif(computer==0 and you ==1):
-        print("you loss!")
-    else:
-        print("something wents wrong")
+    print("😢 You lose!")
